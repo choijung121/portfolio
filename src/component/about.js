f0a010b8ebe5a250164c4b1f0eca3493d46c919d
Jung Choi <56977454+choijung121@users.noreply.github.com>
2022-08-02 09:00:59 -0700

    little changes

--- src/component/about.js
+++ src/component/about.js
@@ -3,11 +3,12 @@ import './about.css'
 function About(){
     return(
         <div>
-            <h1 className="intro-me-title">Hello World</h1>
-            <p1 className="intro-me-summary">Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br></br>
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br></br>
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris <br></br>
-            culpa qui officia deserunt mollit anim id est laborum.</p1>
+            <h1 className="intro-me-title">Hello there!</h1>
+            <p1 className="intro-me-summary">My name is Jung Choi.        
+            <br></br>
+            I work and practice with React.JS to become a better developer and engineer. 
+            There are so many things to learn.
+            </p1>
         </div>
     );
 };
