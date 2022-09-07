c8c173e72248190c0e6f7805dfafce468635b153
Jung Choi <56977454+choijung121@users.noreply.github.com>
2022-09-06 22:41:31 -0700

    added new card

--- src/component/card.js
+++ src/component/card.js
@@ -1,6 +1,7 @@
 import React from "react"; 
 import portfolio from "../images/portfolio.png"
 import expenseApp from "../images/project-expenseapp.png"
+import weatherApp from "../images/weather.png"
 import './card.css'
 
 function Card(){
@@ -50,17 +51,14 @@ function Card(){
             <div class="col-sm-3">
                 <div class="card text-bg-light mb-3">
                     <a href="https://github.com/choijung121/weatherapp" className="card-text">
-                        <img src={expenseApp} class="card-img-top" alt="Example of Weather App"/>
+                        <img src={weatherApp} class="card-img-top" alt="Example of Weather App"/>
                         <div class="card-body">
                             <h5 class="card-title">Weather App</h5>
-                            <p class="card-text">Able to search the city to get information on temperature</p>
+                            <p class="card-text">Simple searchable weather app to get information on temperature and other informations</p>
                             <a href="https://github.com/choijung121/weatherapp" class="link-dark icon-link">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
                                     <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
                                 </svg>
-                                <div className="progress-button">
-                                    <p1>In Progress</p1>
-                                </div>
                             </a>
                         </div>
                     </a>
